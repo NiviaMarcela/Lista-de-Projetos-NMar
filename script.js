12bd66a0867672ddfb2fd91e5a1f6ce7dbb269ec
NiviaMarcela <nivhmarcelhy@hotmail.com>
2025-06-26 19:09:25 -0300

    Alteração

---
+++ script.js
@@ -0,0 +1,7 @@
+function acessar(){
+        document.getElementById("modalcadastro").style.display="flex";
+    }
+
+function fechar() {
+  document.getElementById("modalcadastro").style.display = "none";
+}
